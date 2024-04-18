21341d2e06cede550995a1885db359f9c52ccd85
kaylatrinh <kayla.trinh.nguyen@gmail.com>
2024-04-17 21:45:13 -0400

    a6

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import session from "express-session";
+
+
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+  })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
 app.use(express.json());
+const port = process.env.PORT || 4000;
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 Lab5(app);
--- Kanbas/Database/courses.js
+++ Kanbas/Database/courses.js
@@ -1,6 +1,6 @@
 export default [
     {
-        "_id": "CS4550",
+        "id": "CS4550",
         "name": "Web Development",
         "number": "CS4550",
         "startDate": "2023-01-10",
@@ -8,7 +8,7 @@ export default [
         "image": "blue-course.png"
     },
     {
-        "_id": "EECE2322",
+        "id": "EECE2322",
         "name": "Digital Design",
         "number": "EECE2322",
         "startDate": "2023-01-10",
@@ -16,7 +16,7 @@ export default [
         "image": "green-course.png"
     },
     {
-        "_id": "IS2000",
+        "id": "IS2000",
         "name": "Information Science",
         "number": "IS2000",
         "startDate": "2023-01-10",
@@ -24,7 +24,7 @@ export default [
         "image": "grey-course.png"
     },
     {
-        "_id": "IS4800",
+        "id": "IS4800",
         "name": "Empirical Research",
         "number": "IS4800",
         "startDate": "2023-01-10",
